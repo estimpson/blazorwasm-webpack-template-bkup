5b9dd551c6a880d34d032672492564927401b1dc
Eric Stimpson <estimpson@fore-thought.com>
2021-02-26 13:01:12 -0500

    add src and update index.html

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import './main.scss';
+import { Tooltip, Toast, Popover } from "bootstrap";
+console.log('Hello World from your main file!');
+var toast = new Toast(document.getElementById("app"));
+toast.show();
